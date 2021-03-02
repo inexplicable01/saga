81dd7692fcd3d69d969f3819b5067bd7cf272f9a
Waichak Luk <waichak.luk@gmail.com>
2021-03-01 21:53:45 -0500

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import *
 # BASE = "http://fatpanda1985.pythonanywhere.com/"
 BASE = "http://127.0.0.1:5000/"
 
-
+# comment
 typeInput='Input'
 typeOutput='Output'
 typeRequired='Required'
