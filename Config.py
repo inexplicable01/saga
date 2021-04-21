2c47714d5efa9ebec07b3d00208f503e997b51f1
jleong13 <69608770+jleong13@users.noreply.github.com>
2021-04-21 01:06:21 -0400

    Revert "Fixed up index box"
    This reverts commit ac544916cb640f0099f52b959864c5b18029a363.

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import *
 import os
-BASE = "http://fatpanda1985.pythonanywhere.com/"
-# BASE = "http://127.0.0.1:5000/"
+# BASE = "http://fatpanda1985.pythonanywhere.com/"
+BASE = "http://127.0.0.1:5000/"
 WorldMapDir = 'ContainerMapWorkDir'
 sagaGuiDir = os.getcwd()
 
--- Graphics/GuiUtil.py
+++ Graphics/GuiUtil.py
@@ -6,18 +6,18 @@ from PyQt5.QtCore import *
 def AddIndexToView(view):
     scene = QGraphicsScene()
     offset = 7
-    indexheight = view.height() / 2
-    indexwidth = view.width()
+    indexheight = view.height() / 5
+    indexwidth = view.width() / 2
 
     text = scene.addText('Input')
-    text.setPos(-indexwidth+15, indexheight)
+    text.setPos(-indexwidth+20, indexheight)
     scene.addRect(0, indexheight + offset, indexwidth * 0.8, indexheight , QPen(Qt.black), QBrush(Qt.yellow))
 
     text = scene.addText('Working')
-    text.setPos(-indexwidth+15, indexheight * 2)
+    text.setPos(-indexwidth+20, indexheight * 2)
     scene.addRect(0, indexheight * 2 + offset, indexwidth * 0.8, indexheight , QPen(Qt.black), QBrush(Qt.blue))
 
     text = scene.addText('Output')
-    text.setPos(-indexwidth+15, indexheight * 3)
+    text.setPos(-indexwidth+20, indexheight * 3)
     scene.addRect(0, indexheight * 3 + offset, indexwidth * 0.8, indexheight, QPen(Qt.black), QBrush(Qt.green))
     view.setScene(scene)
