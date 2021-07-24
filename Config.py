656f5d15da7e153850841038d000279b491dd75b
jleong13 <69608770+jleong13@users.noreply.github.com>
2021-07-24 19:28:57 -0400

    Updated Config
    Changed login to Jimmy

--- Config.py
+++ Config.py
@@ -130,7 +130,7 @@ privatepackagecustomer = {'first_name':'Good',
                    'email':'customer@gmail.com',
                     'password':'passwordCust',
 }
-testerlogin=waichak
+testerlogin=jimmy
 
 
 TEMPCONTAINERFN='temp_containerstate.yaml'
